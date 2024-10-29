6d4ccf298c2a9ce71460e6d93356c27eb827e487
amardeep-soni <amardeepmunni@gmail.com>
2024-10-29 12:26:46 +0545

    getting advice with adviceslip api and showing it using useEffect

--- randomAdvice/src/App.jsx
+++ randomAdvice/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Body from './components/Body'
 
 function App() {
 
   return (
-    <h1 className='text-red-700 text-xl'>Hello World!</h1>
+    <div>
+      <Body />
+    </div>
   )
 }
 
