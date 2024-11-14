edcb5b23e120939a674aec57b3307d37db6a3f90
amardeep-soni <amardeepmunni@gmail.com>
2024-11-14 09:47:56 +0545

    router setup in project and getting all restaurant and showing in card and other pages

--- foodMandu/src/App.jsx
+++ foodMandu/src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css'
-import Header from './components/Header'
-import Home from './components/Home'
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import Restaurants from "./components/Restaurants";
+import RestaurantDetail from "./components/RestaurantDetail";
+import RestaurantLayout from "./components/RestaurantLayout";
 
 function App() {
-
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="restaurants" element={<RestaurantLayout />}>
+            <Route index element={<Restaurants />} />
+            <Route path="details/:id" element={<RestaurantDetail />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- foodMandu/src/components/Home.jsx
+++ foodMandu/src/components/Home.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import Card from './card';
 
 export default function Home() {
-    return (
-        <div className='relative'>
-            <img src="https://www.pngmagic.com/product_images/White-brick-wall-background-image.jpg" className='h-[600px] w-full' />
+    const [restaurantData, setRestaurantData] = useState([]);
 
-            <div className='text-lg absolute top-0 left-0 bottom-0 right-0 flex items-center justify-center flex-col'>
-                <p className='text-2xl text-center font-bold'>Order food from the widest range of <br /> Restaurant</p>
-                <div className='flex mt-4 bg-white rounded-lg px-4 py-2'>
-                    <input type="text" className='rounded-lg focus-visible:outline-none pr-4' placeholder='Restaurants or cuisine' />
-                    <button className='bg-[#FFd000] p-4 rounded-lg'>Find Restaurants</button>
+    const getRestaurantDetail = async () => {
+        const data = await fetch(
+            "https://foodmandu.com/webapi/api/Vendor/GetVendors1?Cuisine=&DeliveryZoneId=1&IsFavorite=false&IsRecent=false&Keyword=&LocationLat=27.7026754&LocationLng=85.3191018&PageNo=1&PageSize=8&SortBy=4&VendorName=&VendorTags=%7B%22FEATURED%22:true%7D&VendorTagsCSV=FEATURED,&filtertags=FEATURED&search_by=restaurant"
+        );
+        const jsonData = await data.json();
+        setRestaurantData(jsonData);
+    };
+
+    useEffect(() => {
+        getRestaurantDetail();
+    }, []);
+
+    return (
+        <div>
+            <div className='relative'>
+                <img src="https://www.pngmagic.com/product_images/White-brick-wall-background-image.jpg" className='h-[600px] w-full' />
+
+                <div className='text-lg absolute top-0 left-0 bottom-0 right-0 flex items-center justify-center flex-col'>
+                    <p className='text-2xl text-center font-bold'>Order food from the widest range of <br /> Restaurant</p>
+                    <div className='flex mt-4 bg-white rounded-lg px-4 py-2'>
+                        <input type="text" className='rounded-lg focus-visible:outline-none pr-4' placeholder='Restaurants or cuisine' />
+                        <button className='bg-[#FFd000] p-4 rounded-lg'>Find Restaurants</button>
+                    </div>
                 </div>
+            </div>
+            <div className=" mx-8 flex justify-between py-4">
+                <p className="font-bold text-xl">FEATURED RESTAURANTS</p>
+                <Link to='restaurants' className="flex items-center font-bold text-xl">
+                    View All
+                    <FaArrowRight className="ml-4" />
+                </Link>
+            </div>
+            <div className="flex flex-wrap gap-8 items-center justify-center my-4">
+                {restaurantData.map((data) => (
+                    <Card key={data.Id} propData={data} />
+                ))}
             </div>
         </div>
     )
