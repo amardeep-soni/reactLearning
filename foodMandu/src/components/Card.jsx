5b8772810585aa0c831df1bb000046247b3f990f
amardeep-soni <amardeepmunni@gmail.com>
2024-11-14 11:15:05 +0545

    restaurants page design and home seach page redirect

--- foodMandu/src/components/Card.jsx
+++ foodMandu/src/components/Card.jsx
@@ -1,42 +1,42 @@
 import { RiMapPin2Fill } from "react-icons/ri";
 import { MdPedalBike } from "react-icons/md";
 import { IoFastFood } from "react-icons/io5";
-
 const Card = (props) => {
-    const { propData } = props;
-    const {
-        VendorListingWebImageName,
-        Name,
-        Address1,
-        DeliveryDistanceStr,
-        CuisineTags,
-    } = propData;
-    return (
-        <div className="border-[1px] shadow-md border-black w-fit rounded-md">
-            <img
-                className="h-52 w-96 rounded-md"
-                src={VendorListingWebImageName}
-                alt=""
-            />
-            <div className="flex flex-col gap-1 p-4">
-                <p>{Name}</p>
-                <div className="flex gap-4">
-                    <div className="flex items-center">
-                        <RiMapPin2Fill />
-                        {Address1}
-                    </div>
-                    <div className="flex items-center">
-                        <MdPedalBike />
-                        {DeliveryDistanceStr}
-                    </div>
-                </div>
-                <div className="flex items-center">
-                    <IoFastFood />
-                    {CuisineTags}
-                </div>
-            </div>
+  const { propData } = props;
+  const {
+    VendorListingWebImageName,
+    Name,
+    Address1,
+    DeliveryDistanceStr,
+    CuisineTags,
+  } = propData;
+  return (
+    <div className="h-80 w-96 flex flex-col border-[1px] shadow-md border-black  rounded-md">
+      <img
+        className="h-1/2 w-full rounded-md grow"
+        src={VendorListingWebImageName}
+        alt=""
+      />
+      <div className="flex flex-col gap-1 p-4">
+        <p>{Name}</p>
+        <div className="flex gap-4">
+          <div className="flex items-center">
+            <RiMapPin2Fill />
+            {Address1}
+          </div>
+          <div className="flex items-center">
+            <MdPedalBike />
+            {DeliveryDistanceStr}
+          </div>
         </div>
-    );
+        <div className="flex items-center overflow-hidden whitespace-nowrap text-ellipsis">
+        <span className=" items-start flex justify-start">
+        <IoFastFood />
+        </span>
+          <span className="ml-1 overflow-hidden whitespace-nowrap text-ellipsis">{CuisineTags}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Card;
--- foodMandu/src/components/Restaurants.jsx
+++ foodMandu/src/components/Restaurants.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Card from "./card";
 
 const Restaurants = () => {
+    const [restaurants, setRestaurants] = useState([])
+
+
+    const fetchData = async () => {
+        const data = await fetch('https://foodmandu.com/webapi/api/Vendor/GetVendors1?Cuisine=&DeliveryZoneId=1&IsFavorite=false&IsRecent=false&Keyword=&LocationLat=27.7026754&LocationLng=85.3191018&PageNo=1&PageSize=12&SortBy=4&VendorName=&VendorTags=%7B%22FEATURED%22:true%7D&search_by=restaurant');
+
+        const jsonData = await data.json();
+
+        setRestaurants(jsonData)
+        console.log(jsonData);
+        console.log(restaurants);
+        
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <div>
-            Restaurants page
+            <div className='p-8 bg-slate-100'>
+                <p className="text-4xl">Restaurants and Stores</p>
+            </div>
+
+            <div className="flex justify-center items-center gap-8  flex-wrap mx-8 py-8">
+                {restaurants.map(restaurant => (
+                    <Card propData={restaurant} key={restaurant.id} />
+                ))}
+            </div>
         </div>
     )
 }
