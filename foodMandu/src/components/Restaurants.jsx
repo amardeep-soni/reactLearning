abd3dbb84f94528889a117fd8940617ad50a3c93
amardeep-soni <amardeepmunni@gmail.com>
2024-11-15 10:00:55 +0545

    getting query and showing modifying url to fetch data

--- foodMandu/src/components/Restaurants.jsx
+++ foodMandu/src/components/Restaurants.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import Card from "./card";
+import { useSearchParams } from "react-router-dom";
 
 const Restaurants = () => {
     const [restaurants, setRestaurants] = useState([])
-
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get('query') || '';
 
     const fetchData = async () => {
-        const data = await fetch('https://foodmandu.com/webapi/api/Vendor/GetVendors1?Cuisine=&DeliveryZoneId=1&IsFavorite=false&IsRecent=false&Keyword=&LocationLat=27.7026754&LocationLng=85.3191018&PageNo=1&PageSize=12&SortBy=4&VendorName=&VendorTags=%7B%22FEATURED%22:true%7D&search_by=restaurant');
+        const data = await fetch(`https://foodmandu.com/webapi/api/Vendor/GetVendors1?Cuisine=&DeliveryZoneId=1&IsFavorite=false&IsRecent=false&Keyword=${query}&LocationLat=27.7026754&LocationLng=85.3191018&PageNo=1&PageSize=12&SortBy=4&VendorName=&VendorTags=%7B%22FEATURED%22:true%7D&search_by=restaurant`);
 
         const jsonData = await data.json();
 
@@ -28,7 +30,7 @@ const Restaurants = () => {
 
             <div className="flex justify-center items-center gap-8  flex-wrap mx-8 py-8">
                 {restaurants.map(restaurant => (
-                    <Card propData={restaurant} key={restaurant.id} />
+                    <Card propData={restaurant} key={restaurant.Id} />
                 ))}
             </div>
         </div>
